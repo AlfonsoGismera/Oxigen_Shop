e0bcccbc319e517a39da2abf045895c19e5c71b7
Alfonso <alfonsofcogismeraperea@gmail.com>
2025-03-14 14:14:06 +0100

    function: changeCurrency

--- js/scroller.js
+++ js/scroller.js
@@ -1,9 +1,9 @@
 export default function initScroller() {
 window.addEventListener("scroll", function () {
     const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-    const scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight;
-    const clientHeight = document.documentElement.clientHeight || window.innerHeight;
-    const scrolled = (scrollTop / (scrollHeight - clientHeight)) * 100;
+    const scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight; // Recoge el valor de la pagina total
+    const clientHeight = document.documentElement.clientHeight || window.innerHeight; // Recoge el valor de la pagina actual
+    const scrolled = (scrollTop / (scrollHeight - clientHeight)) * 100; // Calcula el porcentaje de la pagina que se ha recorrido
   
     document.querySelector(".progress").style.width = scrolled + "%";
   });
